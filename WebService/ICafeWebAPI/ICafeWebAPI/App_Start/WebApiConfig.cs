5431eae6a88b7dfd36bc18f0f9ef033379e4cb1a
ravikumar-rayani <ravikumar.rayani@gmail.com>
2016-07-21 22:52:20 +0530

    to get in Json formatter

--- WebService/ICafeWebAPI/ICafeWebAPI/App_Start/WebApiConfig.cs
+++ WebService/ICafeWebAPI/ICafeWebAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace ICafeWebAPI
@@ -12,9 +13,11 @@ namespace ICafeWebAPI
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{method}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            //To produce JSON format add this line of code
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
